b8a299a34142cced6dbf49e4fce785c896c71b09
Victoria <vickgoularte@gmail.com>
2023-04-10 20:37:55 -0300

    🧹 removing comments.

--- app/src/main/java/com/viclab/ricknmorty/ui/RnmState.kt
+++ app/src/main/java/com/viclab/ricknmorty/ui/RnmState.kt
@@ -40,31 +40,14 @@ class RnmAppState(
             else -> null
         }
 
-    /**
-     * Map of top level destinations to be used in the TopBar, BottomBar and NavRail. The key is the
-     * route.
-     */
     val topLevelDestinations: List<TopLevelDestination> = TopLevelDestination.values().asList()
 
-    /**
-     * UI logic for navigating to a top level destination in the app. Top level destinations have
-     * only one copy of the destination of the back stack, and save and restore state whenever you
-     * navigate to and from it.
-     *
-     * @param topLevelDestination: The destination the app needs to navigate to.
-     */
     fun navigateToTopLevelDestination(topLevelDestination: TopLevelDestination) {
         val topLevelNavOptions = navOptions {
-            // Pop up to the start destination of the graph to
-            // avoid building up a large stack of destinations
-            // on the back stack as users select items
             popUpTo(navController.graph.findStartDestination().id) {
                 saveState = true
             }
-            // Avoid multiple copies of the same destination when
-            // reselecting the same item
             launchSingleTop = true
-            // Restore state when reselecting a previously selected item
             restoreState = true
         }
 
